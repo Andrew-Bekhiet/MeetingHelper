719045481691fc0719f56423294cc3101a4b0519
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2021-09-06 14:31:31 +0200

    feat: add getMembersLive() refactor: service

--- lib/models/data/service.dart
+++ lib/models/data/service.dart
@@ -1,18 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:meetinghelper/models/data/user.dart';
 import 'package:meetinghelper/utils/typedefs.dart';
 
 import '../super_classes.dart';
+import 'person.dart';
 
 class Service extends DataObject with PhotoObject {
   StudyYearRange? studyYearRange;
   DateTimeRange? validity;
   bool showInHistory;
+  String lastEdit;
 
   Service({
     required JsonRef ref,
     required String name,
+    required this.lastEdit,
     this.studyYearRange,
     this.validity,
     this.showInHistory = true,
@@ -20,6 +24,15 @@ class Service extends DataObject with PhotoObject {
   }) : super(ref, name, null) {
     this.hasPhoto = hasPhoto;
     defaultIcon = Icons.miscellaneous_services;
+  }
+
+  static Service empty() {
+    return Service(
+      ref: FirebaseFirestore.instance.collection('Services').doc('null'),
+      name: '',
+      hasPhoto: false,
+      lastEdit: User.instance.uid!,
+    );
   }
 
   static Service fromDoc(JsonDoc doc) =>
@@ -40,22 +53,36 @@ class Service extends DataObject with PhotoObject {
                   end: json['Validity']['To'].toDate())
               : null,
           showInHistory: json['ShowInHistory'],
+          lastEdit: json['LastEdit'],
           hasPhoto: json['HasPhoto'],
         );
 
+  Stream<List<Person>> getMembersLive(
+      {bool descending = false, String orderBy = 'Name'}) {
+    return FirebaseFirestore.instance
+        .collection('Persons')
+        .where('Services', arrayContains: ref)
+        .orderBy(orderBy, descending: descending)
+        .snapshots()
+        .map((p) => p.docs.map(Person.fromQueryDoc).toList());
+  }
+
   @override
-  Service copyWith(
-      {JsonRef? ref,
-      String? name,
-      StudyYearRange? studyYearRange,
-      DateTimeRange? validity,
-      bool? showInHistory}) {
+  Service copyWith({
+    JsonRef? ref,
+    String? name,
+    StudyYearRange? studyYearRange,
+    DateTimeRange? validity,
+    bool? showInHistory,
+    String? lastEdit,
+  }) {
     return Service(
       ref: ref ?? this.ref,
       name: name ?? this.name,
       studyYearRange: studyYearRange ?? this.studyYearRange,
       validity: validity ?? this.validity,
       showInHistory: showInHistory ?? this.showInHistory,
+      lastEdit: lastEdit ?? this.lastEdit,
     );
   }
 
@@ -66,6 +93,7 @@ class Service extends DataObject with PhotoObject {
       'StudyYearRange': studyYearRange?.toJson(),
       'Validity': validity?.toJson(),
       'ShowInHistory': showInHistory,
+      'LastEdit': lastEdit,
       'HasPhoto': hasPhoto,
     };
   }
@@ -76,8 +104,8 @@ class Service extends DataObject with PhotoObject {
 }
 
 class StudyYearRange {
-  JsonRef from;
-  JsonRef to;
+  JsonRef? from;
+  JsonRef? to;
 
   StudyYearRange({required this.from, required this.to});
 
