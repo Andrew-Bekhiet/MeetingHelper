24022285eb6ebafb89a403e679fae810013fe85d
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2022-06-18 16:28:28 +0200

    rename: list.dart -> day_check_list.dart

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,6 +1,6 @@
 export 'widgets/analytics_indicators.dart';
+export 'widgets/day_check_list.dart';
 export 'widgets/history_property_widgets.dart';
-export 'widgets/list.dart';
 export 'widgets/loading_widget.dart';
 export 'widgets/notification_widget.dart';
 export 'widgets/search_filters.dart';
