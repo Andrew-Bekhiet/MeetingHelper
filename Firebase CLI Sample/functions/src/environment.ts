408d2638a806726e70e9397fb0832445c691e098
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2022-08-31 16:46:51 +0200

    fix(backend): reserved env vars

--- Firebase CLI Sample/functions/src/environment.ts
+++ Firebase CLI Sample/functions/src/environment.ts
@@ -1,8 +1,7 @@
 export const adminPassword = process.env.ADMIN_PASSWORD;
-export const firebase_dynamic_links_key =
-  process.env.FIREBASE_DYNAMIC_LINKS_KEY!;
+export const firebase_dynamic_links_key = process.env.FB_DYNAMIC_LINKS_KEY!;
 export const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
 export const packageName =
   process.env.PACKAGE_NAME ?? "com.AndroidQuartz.meetinghelper";
 export const firebase_dynamic_links_prefix =
-  process.env.FIREBASE_DYNAMIC_LINKS_PREFIX!;
+  process.env.FB_DYNAMIC_LINKS_PREFIX!;
