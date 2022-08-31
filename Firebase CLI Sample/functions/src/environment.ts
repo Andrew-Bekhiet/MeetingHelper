91d4fc9c5d541658ded3c1b27dd7bb6ee24046ac
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2022-08-31 17:13:48 +0200

    feat(backend): add default admin password

--- Firebase CLI Sample/functions/src/environment.ts
+++ Firebase CLI Sample/functions/src/environment.ts
@@ -1,4 +1,4 @@
-export const adminPassword = process.env.ADMIN_PASSWORD;
+export const adminPassword = process.env.ADMIN_PASSWORD ?? "p^s$word";
 export const firebase_dynamic_links_key = process.env.FB_DYNAMIC_LINKS_KEY!;
 export const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
 export const packageName =
