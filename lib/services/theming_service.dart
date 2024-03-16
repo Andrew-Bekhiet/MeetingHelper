d24a3ed735c69dcfb2af06ab5269fb5fa26613a5
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2024-03-16 23:00:51 +0200

    ui: don't use material 3 yet

--- lib/services/theming_service.dart
+++ lib/services/theming_service.dart
@@ -1,16 +1,94 @@
 import 'package:churchdata_core/churchdata_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
 
 class MHThemingService extends ThemingService {
   static ThemeData getDefault({
     bool? darkTheme,
     bool? greatFeastThemeOverride,
   }) {
-    return ThemingService.getDefault(
-      primaryOverride: Colors.amber,
-      secondaryOverride: Colors.amberAccent,
-      darkTheme: darkTheme,
-      greatFeastThemeOverride: greatFeastThemeOverride,
+    bool isDark = darkTheme ??
+        GetIt.I<CacheRepository>().box('Settings').get('DarkTheme') ??
+        PlatformDispatcher.instance.platformBrightness == Brightness.dark;
+
+    final bool greatFeastTheme = GetIt.I<CacheRepository>()
+        .box('Settings')
+        .get('GreatFeastTheme', defaultValue: greatFeastThemeOverride ?? true);
+
+    MaterialColor primary = Colors.amber;
+    Color secondary = Colors.amberAccent;
+
+    final riseDay = getRiseDay();
+    if (greatFeastTheme &&
+        DateTime.now()
+            .isAfter(riseDay.subtract(const Duration(days: 7, seconds: 20))) &&
+        DateTime.now().isBefore(riseDay.subtract(const Duration(days: 1)))) {
+      primary = ThemingService.black;
+      secondary = ThemingService.blackAccent;
+      isDark = true;
+    } else if (greatFeastTheme &&
+        DateTime.now()
+            .isBefore(riseDay.add(const Duration(days: 50, seconds: 20))) &&
+        DateTime.now().isAfter(riseDay.subtract(const Duration(days: 1)))) {
+      isDark = false;
+    }
+
+    return ThemeData.from(
+      useMaterial3: false,
+      colorScheme: ColorScheme.fromSwatch(
+        backgroundColor: isDark ? Colors.grey[850]! : Colors.grey[50]!,
+        brightness: isDark ? Brightness.dark : Brightness.light,
+        primarySwatch: primary,
+        accentColor: secondary,
+      ),
+    ).copyWith(
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+          borderSide: BorderSide(color: primary),
+        ),
+      ),
+      floatingActionButtonTheme:
+          FloatingActionButtonThemeData(backgroundColor: primary),
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      brightness: isDark ? Brightness.dark : Brightness.light,
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+      ),
+      appBarTheme: AppBarTheme(
+        backgroundColor: primary,
+        foregroundColor: (isDark
+                ? Typography.material2018().white
+                : Typography.material2018().black)
+            .titleLarge
+            ?.color,
+        systemOverlayStyle:
+            isDark ? SystemUiOverlayStyle.light : SystemUiOverlayStyle.dark,
+      ),
+      bottomAppBarTheme: BottomAppBarTheme(
+        color: secondary,
+        shape: const CircularNotchedRectangle(),
+      ),
     );
   }
 
